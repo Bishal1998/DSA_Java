9bb2225126258df083fb3ec87f9c1c342a49a2fd
bishal1998 <bishalpaudel1998@gmail.com>
2024-11-11 11:10:59 -0500

    Find the element in the Fibonacci Series

---
+++ Array/Lecture17/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package Array.Lecture17;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    // Find the specific elements from the fibonacci series.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println(arr[n-1]);
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. [Search Element in an array](https://github.com/Bishal1998/DSA_Java/blob/main/Array/Lecture17/SearchElement.java)
 2. [Reverse an Array](https://github.com/Bishal1998/DSA_Java/blob/main/Array/Lecture17/ReverseArray.java)
 3. [Second Maximum](https://github.com/Bishal1998/DSA_Java/blob/main/Array/Lecture17/SecondMaximum.java)
-4. [MissingNumber]
+4. [MissingNumber](https://github.com/Bishal1998/DSA_Java/blob/main/Array/Lecture17/MissingNumber.java)
+5. [Fibonacci Series]
